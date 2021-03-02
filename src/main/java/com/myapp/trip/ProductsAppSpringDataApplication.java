caedfb0bc5e50c8347ab87e05ec60f461c555761
Saloni-19 <Salonikhandelwal920@gmail.com>
2021-03-01 22:45:54 -0800

    First Commit

--- src/main/java/com/myapp/trip/ProductsAppSpringDataApplication.java
+++ src/main/java/com/myapp/trip/ProductsAppSpringDataApplication.java
@@ -1,4 +1,4 @@
-package com.myapp.spring;
+package com.myapp.trip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/myapp/trip/ProductsAppSpringDataApplicationTests.java
+++ src/test/java/com/myapp/trip/ProductsAppSpringDataApplicationTests.java
@@ -1,4 +1,4 @@
-package com.myapp.spring;
+package com.myapp.trip;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
